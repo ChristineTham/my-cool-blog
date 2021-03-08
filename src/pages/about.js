f0194493e476b943cbd1681381c8003c1824eb87
Christine Tham <chris.tham@hellotham.com>
2021-03-08 21:00:21 +1100

    Use and style React components

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const About = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hello World!</p>
+    </Layout>
+  );
+};
+
+export default About;
