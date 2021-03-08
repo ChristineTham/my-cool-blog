fb4277b8c734d7792b3ce86ba8b234908474dd7b
Christine Tham <chris.tham@hellotham.com>
2021-03-09 09:32:37 +1100

    Query for data with GraphQL

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
-} from './layout.module.css'
+  navLinkText,
+  siteTitle,
+} from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
+
   return (
     <main className={container}>
-      <title>{pageTitle}</title>
+      <title>{data.site.siteMetadata.title}</title>
+      <p className={siteTitle}>{data.site.siteMetadata.title}</p>
+
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
@@ -24,12 +38,17 @@ const Layout = ({ pageTitle, children }) => {
               About
             </Link>
           </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
+          </li>
         </ul>
       </nav>
       <h1 className={heading}>{pageTitle}</h1>
       {children}
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
