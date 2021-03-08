7cb942bb6ceb8638b8ec3cddb78f2ddb8daaaf72
Christine Tham <chris.tham@hellotham.com>
2021-03-09 09:52:22 +1100

    Transform data to use Markdown

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       <ul>
-        {data.allFile.nodes.map((node) => {
-          return <li key={node.name}>{node.name}</li>;
+        {data.allMdx.nodes.map((node) => {
+          return (
+            <li key={node.frontmatter.title}>
+              <article>
+                <h2>{node.frontmatter.title}</h2>
+                <p>Posted: {node.frontmatter.date}</p>
+                <MDXRenderer>{node.body}</MDXRenderer>
+              </article>
+              <hr />
+            </li>
+          );
         })}
       </ul>
     </Layout>
@@ -16,9 +26,13 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query BlogPosts {
-    allFile(filter: { dir: { regex: "/blog/" } }) {
+    allMdx(sort: { order: DESC, fields: frontmatter___date }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "YYYY-MMMM-DD")
+          title
+        }
+        body
       }
     }
   }
